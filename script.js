c99d57c75187f5869ce119773b51d098c20b9c15
Ishita patil <patilishita2007@gmail.com>
2026-04-07 23:45:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// smooth glow animation
-const glow = document.querySelector(".glow");
-
-window.addEventListener("mousemove", (e) => {
-  glow.style.left = e.pageX + "px";
-  glow.style.top = (e.pageY - 200) + "px";
-});

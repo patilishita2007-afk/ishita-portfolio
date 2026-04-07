017d1954a63455b4fc007470341c1d2242821d3a
Ishita patil <patilishita2007@gmail.com>
2026-04-07 23:04:50 +0530

    added

---
+++ script.js
@@ -0,0 +1,7 @@
+// smooth glow animation
+const glow = document.querySelector(".glow");
+
+window.addEventListener("mousemove", (e) => {
+  glow.style.left = e.pageX + "px";
+  glow.style.top = (e.pageY - 200) + "px";
+});

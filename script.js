c4a290001aba2c565c24f0df84faa173fe54c66e
Ishita patil <patilishita2007@gmail.com>
2026-05-02 20:38:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const glow = document.querySelector(".glow");
+// Typing Animation
+const words = [
+  "Flutter Developer",
+  "Web Developer",
+  "Cyber Security Enthusiast",
+  "Tech Learner"
+];
 
-window.addEventListener("mousemove", (e) => {
-  glow.style.left = e.pageX + "px";
-  glow.style.top = (e.pageY - 200) + "px";
-});
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+  let currentWord = words[wordIndex];
+
+  if (!isDeleting) {
+    typingElement.textContent = currentWord.substring(0, charIndex + 1);
+    charIndex++;
+  } else {
+    typingElement.textContent = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+  }
+
+  let speed = 120;
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    speed = 1200;
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    speed = 400;
+  }
+
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
